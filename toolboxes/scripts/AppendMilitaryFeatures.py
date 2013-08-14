7ffbbbc8a3dad796d909a25a919b8d7e3d410a3b
C Moore <csmooreacm@acm.org>
2013-08-14 08:28:26 -0400

    Fix to Issue #9 (Part 3)

--- toolboxes/scripts/AppendMilitaryFeatures.py
+++ toolboxes/scripts/AppendMilitaryFeatures.py
@@ -49,10 +49,10 @@ try:
     # AppendMilitaryFeatures.exe <inputFeatureClass> <outputGDB> [sicField]
     currentPath = os.path.dirname(__file__)
     EXE_PATH = os.path.normpath(os.path.join(currentPath, r"../../application/"))
-    EXE_PATH_AND_FILE = os.path.join(EXE_PATH, r"AppendMilitaryFeatures.exe")
+    EXE_PATH_AND_FILE = "\"" + os.path.join(EXE_PATH, r"AppendMilitaryFeatures.exe") + "\""
     EXE_CMD = EXE_PATH_AND_FILE + r" " + arguments
-    LOG_FILENAME = os.path.join(EXE_PATH, r"log.txt")
-    COMMAND_LINE = EXE_CMD + r" > " + LOG_FILENAME    
+    LOG_FILENAME = os.path.normpath(os.path.join(EXE_PATH, r"log.txt"))
+    COMMAND_LINE = EXE_CMD + r" > " + "\"" + LOG_FILENAME + "\""
     # Ex: COMMAND_LINE=r"AppendMilitaryFeatures.exe arg1 arg2 arg3 > c:/temp/log.txt"
     
     # Run the command at a command prompt
